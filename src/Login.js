7a7bbba26783eaa2a1fd16cb65a211f750b08daf
Brayssen <btorrejoncerna@gmail.com>
2020-11-13 19:38:05 -0600

    Version 5

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
+import image1 from './Assets/Images/image1.svg'
+import image2 from './Assets/Images/image2.svg'
 
-export default function Login() {
+export default function Login({updateModeDefault = false}) {
+    const [updateMode, setUpdateMode] = useState(updateModeDefault);
+
+
     return (
-        <div className="container">
+        <div className={updateMode ? 'container sign-up-mode' : 'container'}>
             <div className="forms-container">
                 <div className="signin-signup">
                     <form action="" className="sign-in-form">
@@ -50,7 +55,26 @@ export default function Login() {
                 </div>
             </div>
             <div className="panels-container">
+                <div className="panel left-panel">
+                    <div className="content">
+                        <h3>New here ?</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus impedit quidem quibusdam?</p>
+                        <button className="btn transparent" id="sign-up-btn" aria-label="updateMode on/off"
+                        onClick={() => setUpdateMode(!updateMode)} onChange={() => setUpdateMode(!updateMode)}>Sign up</button>
+                    </div>
 
+                    <img src={image1} className="image" alt=""/>
+                </div>
+
+                <div className="panel right-panel">
+                    <div className="content">
+                        <h3>One of us ?</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus impedit quidem quibusdam?</p>
+                        <button className="btn transparent" id="sign-in-btn" aria-label="updateMode off" onClick={() => setUpdateMode(!updateMode)} onChange={() => setUpdateMode(!updateMode)} >Sign in</button>
+                    </div>
+
+                    <img src={image2} className="image" alt=""/>
+                </div>
             </div>
         </div>
     )
